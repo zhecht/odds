ac9374247677b0f1879c24f28e05e064a3c38774
Zack Hecht <zhecht7@gmail.com>
2025-06-28 12:47:57 -0400

    test

--- dingers.py
+++ dingers.py
@@ -522,8 +522,8 @@ def writeCirca(date):
 	for pageIdx, page in enumerate(pages):
 		page.save(f"out-{pageIdx}.png", "PNG")
 		img = Image.open(f"out-{pageIdx}.png")
-		bottom, top = 2330, 325
-		left, right = 275, 510
+		bottom, top = 2345, 315
+		left, right = 310, 530
 
 		#bottom, top = 2455, 374
 		#left,right = 145, 440
@@ -574,14 +574,14 @@ def writeCirca(date):
 		#i = img.crop((770,1230,1035,1320))
 		#print(pytesseract.image_to_string(i).split("\n"))
 
-		l,r = 595,640
+		l,r = 610,650
 		if pageIdx == 1:
 			l,r = 590,635
 			pass
 		oversImg = img.crop((l,top,r,bottom))
 		oversImg.save("out-overs.png", "PNG")
 
-		l,r = 710,755
+		l,r = 715,760
 		if pageIdx == 1:
 			l,r = 705,755
 			pass
